80f6bf3349814e606c8529caace0c4e0004d151c
magiran <andreycu@mail.ru>
2021-04-14 16:10:29 +0300

    1.10 model's objects on site as plaintext

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from bboard.models import Bb
 
 
 def index(request):
-    return HttpResponse('Здесь будет выведен список объявлений.')
+    html = 'Список объявлений\r\n\r\n\r\n'
+    for b in Bb.objects.order_by('-published'):
+        html += f'{b.title}\r\n{b.content}\r\n\r\n'
+    return HttpResponse(html, content_type='text/plain; charset=utf-8')
