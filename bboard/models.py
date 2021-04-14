dfba6b0d2e008ec4633fe170619eab6064d20f08
AndreySK <andreycu@mail.ru>
2021-04-14 22:47:07 +0300

    2.1 link one-to-many in models

--- bboard/models.py
+++ bboard/models.py
@@ -6,8 +6,18 @@ class Bb(models.Model):
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
     class Meta:
         verbose_name_plural = 'Объявления'  # название элементов модели во множественном числе
         verbose_name = 'Объявление'  # в единственном числе
         ordering = ['-published']  # сортировка по умолчанию
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
