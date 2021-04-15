72c96325869a5e592160b87fdc078043c5d40fd4
magiran <andreycu@mail.ru>
2021-04-15 09:32:56 +0300

    2.2 __str__ for model

--- bboard/admin.py
+++ bboard/admin.py
@@ -4,7 +4,7 @@ from .models import Bb, Rubric
 
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'published')  # какие колонки выводить в таблице админ панели
+    list_display = ('title', 'content', 'price', 'published', 'rubric')  # какие колонки выводить в таблице админ панели
     list_display_links = ('title', 'content')  # на какие назначить ссылки для перехода на редактирование элемента
     search_fields = ('title', 'content')  # по каким полям искать (появится строка для поиска)
 
