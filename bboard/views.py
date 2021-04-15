34c96855cedd754732fcf49628eaa3de5b5d2f94
magiran <andreycu@mail.ru>
2021-04-15 10:46:51 +0300

    2.3 parameters in URL

--- bboard/views.py
+++ bboard/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from bboard.models import Bb
+from bboard.models import Bb, Rubric
 
 
 def index(request):
     context = {
-        'bbs': Bb.objects.all()
+        'bbs': Bb.objects.all(),
+        'rubrics': Rubric.objects.all(),
     }
 
     return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    context = {
+        'bbs': Bb.objects.filter(rubric=rubric_id),
+        'rubrics': Rubric.objects.all(),
+        'current_rubric': Rubric.objects.get(pk=rubric_id),
+    }
+
+    return render(request, 'bboard/by_rubric.html', context)
