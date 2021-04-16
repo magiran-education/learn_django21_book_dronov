3fc7052c7d64d079a7040dc213b98ab7c5dc0a86
magiran <andreycu@mail.ru>
2021-04-16 10:46:50 +0300

    2.5-2.6 form (ModelForm) and controller-view CBV (CreateView)

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from bboard.models import Bb, Rubric
+from bboard.forms import BbForm
 
 
 def index(request):
@@ -19,3 +23,15 @@ def by_rubric(request, rubric_id):
     }
 
     return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+
+        return context
